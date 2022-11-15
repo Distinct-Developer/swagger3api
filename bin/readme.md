a576ec823a55211bd0c81cbba94d9cbd39838022
Distinct Developer <developer@distincttech.co.za>
2022-11-15 13:26:58 +0200

    Improvement changes.

---
+++ bin/readme.md
@@ -0,0 +1,18 @@
+### Spring Boot REST app with Swagger 3 API Documentation ###
+
+## Swagger 3 API Docs
+http://localhost:8080/swagger3/v3/api-docs
+
+## Swagger 3 API UI
+http://localhost:8080/swagger3/swagger-ui/index.html
+
+## REST API
+## POST
+localhost:8080/swagger3/contacts
+
+## GET All Contacts
+localhost:8080/swagger3/contacts
+
+## GET Contact By ID
+localhost:8080/swagger3/contacts/1
+
--- src/main/java/za/co/distinct/swagger/api/resources/AddressBookResource.java
+++ src/main/java/za/co/distinct/swagger/api/resources/AddressBookResource.java
@@ -1,10 +1,16 @@
 
 package za.co.distinct.swagger.api.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,23 +25,41 @@ import za.co.distinct.swagger.api.models.Contact;
  */
 @RestController
 @RequestMapping("contacts")
+@Log4j2
 public class AddressBookResource {
     
-    private ConcurrentMap<String, Contact> contactList = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, Contact> contactList = new ConcurrentHashMap<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Logger logger = LogManager.getLogger(AddressBookResource.class);
     
     @GetMapping("/{id}")
-    public Contact getContact(@PathVariable("id") String id){
+    public Contact getContact(@PathVariable("id") String id) throws JsonProcessingException {
+        String contactJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(contactList.get(id));
+        logger.info("GET Contact By ID: "+ contactJson);
         return contactList.get(id);
     }
     
     @GetMapping
-    public List<Contact> getAllContacts(){
-        return new ArrayList<>(contactList.values());
+    public Collection<Contact> getAllContacts() throws JsonProcessingException {
+        String contactListJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(contactList.values());
+        logger.info("GET All Contacts: "+ contactListJson);
+        return contactList.values();
     }
     
     @PostMapping
-    public Contact addContact(@RequestBody Contact contact){
+    public Contact addContact(@RequestBody Contact contact) throws JsonProcessingException {
         contactList.put(contact.getId(), contact);
+        String contactJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(contact);
+        logger.info("POST Contact: "+ contactJson);
         return contact;
+    }
+    
+    @PostMapping("/all")
+    public Collection<Contact> saveAllContacts(@RequestBody Collection<Contact> newContactList) throws JsonProcessingException {        
+        ConcurrentHashMap<String, Contact> newContactsMap = newContactList.stream().collect(Collectors.toMap(Contact::getId, Function.identity(), (contact, nextContact) -> contact, ConcurrentHashMap::new));
+        String contactJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(newContactsMap);
+        logger.info("POST All Contacts: "+ contactJson);
+        this.contactList.putAll(newContactsMap);
+        return newContactList;
     }
 }
